13c16333fd6e4b21b3620a6d0600784b50f9d866
Cameron Beamon <BeamonCameron@gmail.com>
2022-01-10 12:04:20 -0600

    added new project

---
+++ array_hash_practice.rb
@@ -0,0 +1,10 @@
+#  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
+#     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+
+array = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
+new_hash = {}
+
+i = 0
+
+while i < array.length
+end
