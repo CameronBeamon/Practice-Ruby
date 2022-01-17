3ed4ab26723b2a25376877f56cb6a0aaa91089de
Cameron Beamon <BeamonCameron@gmail.com>
2022-01-17 17:50:19 -0600

    answered the question using the select method

--- array_hash_practice.rb
+++ array_hash_practice.rb
@@ -44,10 +44,12 @@
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 array = [2, 4, 5, 1, 8, 9, 7]
-new_array = []
-array.each do |number|
-  if number % 2 == 0
-    new_array << number
-  end
-end
-p new_array
+# new_array = []
+# array.each do |number|
+#   if number % 2 == 0
+#     new_array << number
+#   end
+# end
+# p new_array
+e = array.select { |even_num| even_num.even? }
+p e
